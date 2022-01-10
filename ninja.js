fdeda478c1af8aa2ad188580701a50526243ba7a
weicheng lee <z82111712@gmail.com>
2022-01-09 22:05:09 -0800

    update super parameter

--- ninja.js
+++ ninja.js
@@ -27,8 +27,8 @@ ninja1.showStats();
 
 class Sensei extends Ninja {
     constructor(name) {
-        super(name, 200, 10, 10);
-        this.wisdom = 10;
+        super(name, 200, 10, 10, wisdom=10);
+        this.wisdom = wisdom;
     }
     speakWisdom() {
         super.drinkSake();
